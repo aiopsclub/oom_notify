a25867c45115bd4f999e872b20afada2c3922269
yuanxiongxiong <yuanxiongxiong@genedock.com>
2020-03-04 13:02:37 +0800

    change logfile to /dev/kmsg

--- go.mod
+++ go.mod
@@ -3,8 +3,7 @@ module oom_notify
 go 1.14
 
 require (
-	github.com/hpcloud/tail v1.0.0
+	github.com/euank/go-kmsg-parser v2.0.0+incompatible
 	golang.org/x/sys v0.0.0-20200302150141-5c8b2ff67527 // indirect
-	gopkg.in/fsnotify.v1 v1.4.7 // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 )
--- main.go
+++ main.go
@@ -3,46 +3,54 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/hpcloud/tail"
+	"github.com/euank/go-kmsg-parser/kmsgparser"
 	"oom_notify/utils/notify"
 	"oom_notify/utils/oom_match"
 	"os"
 )
 
 var dingdingtoken string
-var logfile string
-var msg string
+var result_msg string
 
 func main() {
 	hostname, err_get_host := os.Hostname()
 	if err_get_host != nil {
 		fmt.Println("get hostname error...")
-		os.Exit(1)
+		os.Exit(2)
 	}
 	// 设置命令行参数
 	flag.StringVar(&dingdingtoken, "token", "", "dingding token")
-	flag.StringVar(&logfile, "logfile", "/dev/kmsg", "system log file")
 	flag.Parse()
 
 	if len(dingdingtoken) <= 0 {
 		fmt.Println("dingdingtoken is too short...")
-		os.Exit(1)
+		os.Exit(3)
 	}
 	fmt.Println("oom notify start...")
-	t, err := tail.TailFile(logfile, tail.Config{Follow: true, ReOpen: true})
+
+	parser, err := kmsgparser.NewParser()
+
 	if err != nil {
-		fmt.Printf("oom notify start failed. reason: %s", err)
-		os.Exit(1)
+		fmt.Printf("unable to create parser: %v", err)
+		os.Exit(4)
 	}
-	for line := range t.Lines {
-		oom_result := oom_match.OomAnalyse(line.Text)
+	defer parser.Close()
+	parse_err := parser.SeekEnd()
+	if parse_err != nil {
+		fmt.Printf("could not tail: %v", err)
+		os.Exit(5)
+	}
+	kmsg := parser.Parse()
+
+	for msg := range kmsg {
+		oom_result := oom_match.OomAnalyse(msg.Message)
 		if oom_result.IsOom {
 			if oom_result.IsCgroup {
-				msg = fmt.Sprintf("%s oom happend. type: Cgroup", hostname)
+				result_msg = fmt.Sprintf("%s oom happend. type: Cgroup", hostname)
 			} else {
-				msg = fmt.Sprintf("%s oom happend. type: System", hostname)
+				result_msg = fmt.Sprintf("%s oom happend. type: System", hostname)
 			}
-			notify.Dingding(dingdingtoken, msg)
+			notify.Dingding(dingdingtoken, result_msg)
 		}
 
 	}
